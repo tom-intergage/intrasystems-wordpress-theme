a6db05dcc86ad87a68511dc61bd406e04df4478c
tom-intergage <39376979+tom-intergage@users.noreply.github.com>
2020-01-21 10:27:56 +0000

    Update header.php

--- header.php
+++ header.php
@@ -40,6 +40,21 @@
 
   <!-- End Google Tag Manager -->
 
+  <script type="text/javascript">
+_linkedin_partner_id = "1840233";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+</script><script type="text/javascript">
+(function(){var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})();
+</script>
+<noscript>
+<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=1840233&fmt=gif" />
+</noscript>
+
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="http://gmpg.org/xfn/11">
